5f2e241c69ed185069ade9c19a23c30c5af42f61
Jimwell Parinas <jp.pagapulan@gmail.com>
2022-01-20 13:14:00 +0800

    add textarea

--- resources/views/components/ui/form/textarea.blade.php
+++ resources/views/components/ui/form/textarea.blade.php
@@ -6,5 +6,5 @@
 
 <div class="mb-3">
     <label class="form-label">{{ $label }}</label>
-    <textarea class="form-control {{ $class }}" {{ $attributes }} cols="{{ $size[0] ?? 30 }}" rows="{{ $size[1] ?? 10 }}">{{ $slot }}</textarea>
+    <textarea class="form-control {{ $class ?? '' }}" {{ $attributes }} cols="{{ $size[0] ?? 30 }}" rows="{{ $size[1] ?? 10 }}">{{ $slot }}</textarea>
 </div>
